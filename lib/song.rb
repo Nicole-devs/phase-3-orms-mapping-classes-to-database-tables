76ac0a2e42f08f80e170432a9f7a26c7eb85a44d
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-05-26 09:40:30 +0300

    def save

--- lib/song.rb
+++ lib/song.rb
@@ -19,6 +19,19 @@ class Song
     SQL
     DB [:conn].execute (sql)
   end
+
+
+  def save 
+    sql = <<- SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    SQL
+    
+    DB [:conn].execute(sql, self.name, self.album)
+  end
+
+end
+
 end
 
 
